1e0d2fb7998f8f550cee38f2dac5ef6e015291d5
shortstack <whitney.ellis.champion@gmail.com>
2019-05-24 12:02:06 -0400

    log

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     API_KEY=os.environ.get('HIVE_SECRET_KEY')
     HIVE_URL='https://myhive.mycompany.com'
-    LOG_FILE='/var/log/mailgun2hive.log'
+    LOG_FILE='/var/log/mailgun2thehive.log'
